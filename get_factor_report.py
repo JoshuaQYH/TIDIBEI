894aebe6d8be8ef6257dd8189db7d49f673ae426
unknown <1196054135@qq.com>
2019-04-18 02:31:07 +0800

    modified run_test

--- get_factor_report.py
+++ get_factor_report.py
@@ -1,13 +1,15 @@
 import atrader as at
 import pandas as pd
 import numpy as np
+import sys
 """
 运行之前！！！！！！！！！！！
 修改输出的csv文件名！！！！！！
 注意不要重复，可能会覆盖原来的文件！！！
 """
-csv_file = "strategy3.csv"
 
+class_name = sys.argv[1]
+csv_file = class_name + ".csv"
 strategy_dicts = at.get_strategy_id()
 save_dict = {"测试因子": [],
                  '年化收益率': [],
@@ -21,8 +23,8 @@ for strategy in strategy_dicts:
     strategy_id = strategy["strategy_id"]
     result = at.get_performance(strategy_id)
     save_dict['测试因子'].append(result['strategy_name'])
-    save_dict['年化收益率'].append(result['annu_return']*np.sqrt(12))
-    save_dict['年化夏普率'].append(result['sharpe_ratio'])
+    save_dict['年化收益率'].append(result['annu_return'])
+    save_dict['年化夏普率'].append(result['sharpe_ratio']*np.sqrt(12))
     save_dict['最大回撤率'].append(result['max_drawback_rate'])
     save_dict['alpha'].append(result['alpha'])
     save_dict['beta'].append(result['beta'])
